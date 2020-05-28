e8b06a9a2a5f2efe9ad57d6f7631d5b2196bc3f7
looooo <sppedflyer@gmail.com>
2020-05-28 13:53:38 +0200

    connect check-updates button

--- freecad/appimage_updater/init_gui.py
+++ freecad/appimage_updater/init_gui.py
@@ -5,9 +5,24 @@ import FreeCADGui
 
 from . update import AppImageUpdaterDialog
 
-ui_file = os.path.join(os.path.dirname(__file__), "ui", "preferences.ui")
-FreeCADGui.addPreferencePage(ui_file,'AppImage')
+DIR = os.path.dirname(__file__)
+FreeCADGui.addIconPath(os.path.join(DIR, "icons"))
+
+class AppImagePreferencePage:
+    def __init__(self):
+        ui_file = os.path.join(DIR, "ui", "preferences.ui")
+        self.form = FreeCADGui.PySideUic.loadUi(ui_file)
+        self.form.check_updates_button.clicked.connect(self.show_updater)
+
+    def show_updater(*args):
+        FreeCAD.appimage_updater = AppImageUpdaterDialog()
+        FreeCAD.appimage_updater.exec()
+
+FreeCADGui.addPreferencePage(AppImagePreferencePage,'AppImage')
+
+
 preference_tabel = FreeCAD.ParamGet('User parameter:BaseApp/Preferences/AppImage')
 appimage_auto_update = preference_tabel.GetBool("appimage_auto_update", False)
+
 if appimage_auto_update:
     FreeCAD.appimage_updater = AppImageUpdaterDialog()
