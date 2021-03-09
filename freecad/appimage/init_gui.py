cea2c02f87be4dd7cb73acda0160a473bdacc683
antony-jr <antonyjr@protonmail.com>
2021-03-10 01:03:43 +0530

    Use QAppImageUpdate GUI for updater and use version v2.0.0

--- freecad/appimage/init_gui.py
+++ freecad/appimage/init_gui.py
@@ -1,9 +1,10 @@
+import sys
+import time
 import os
+from PySide2 import QtWidgets, QtGui, QtCore
 
 import FreeCAD
 import FreeCADGui
-
-from . update import AppImageUpdaterDialog
 
 DIR = os.path.dirname(__file__)
 FreeCADGui.addIconPath(os.path.join(DIR, "icons"))
@@ -12,11 +13,37 @@ class AppImagePreferencePage:
     def __init__(self):
         ui_file = os.path.join(DIR, "ui", "preferences.ui")
         self.form = FreeCADGui.PySideUic.loadUi(ui_file)
-        self.form.check_updates_button.clicked.connect(self.show_updater)
+        self.form.check_updates_button.clicked.connect(self.start_updater)
 
-    def show_updater(*args):
-        FreeCAD.appimage_updater = AppImageUpdaterDialog()
-        FreeCAD.appimage_updater.exec()
+        # importing the plugin and creating an instance
+        self.loader = QtCore.QPluginLoader()
+        self.loader.setFileName("libQAppImageUpdate")
+        self.loaded = self.loader.load()
+        if self.loaded:
+            self.updater = self.loader.instance()
+            self.updater.quit.connect(self.quit_freecad)
+
+    def quit_freecad(self):
+        FreeCADGui.getMainWindow().close()
+
+    def start_updater(self):
+        if not self.loaded:
+            return
+
+        self.updater.cancel()
+        self.updater.clear()
+        
+
+        # Get Icon for FreeCAD.
+        icon = QtCore.QByteArray()
+        buf = QtCore.QBuffer(icon)
+        buf.open(QtCore.QIODevice.WriteOnly);
+        pixmap = QtGui.QPixmap(":/icons/freecad.svg")
+        pixmap.save(buf, "PNG")
+
+        self.updater.setApplicationName("FreeCAD")
+        self.updater.setIcon(icon)
+        self.updater.start(self.updater.getConstant("Action::UpdateWithGUI"))
 
     def saveSettings(self):
         pass
